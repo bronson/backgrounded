b147182d9baa265e19649a7c88932d8c9b13b42d
Scott Bronson <brons_git@rinspin.com>
2015-07-20 23:34:58 -0700

    switch to using bashes repo for testing

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 status=0
+test=test/run-tests.sh
 
-for b in $BASHES; do
-    echo
-    echo ==================
-    echo $b
-    echo ==================
-    $b test/run-tests.sh || status=$?
+# download the bash binaries
+for version in $BASHES; do
+  wget -nv https://raw.githubusercontent.com/bronson/bashes/master/$(arch)/bash-$version
+  [ ! -f bash-$version ] && exit 1
+  chmod a+x bash-$version
+done
+
+# run the tests on each binary
+for version in $BASHES; do
+  echo
+  echo ==================
+  echo Bash $version
+  echo ==================
+  ./bash-$version "$test" || status=$?
 done
 
 exit $status
